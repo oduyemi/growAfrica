e5a8ff09a494c2e56ce34c1037e5993254d5f1a1
Yemi Oduyemi <yhermii@gmail.com>
2024-04-02 12:45:02 +0100

    sign in form updated

--- src/components/SigninForm.jsx
+++ src/components/SigninForm.jsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/material";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import Button from "../elements/Button";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export const SigninForm = () => {
     const { handleLogin, flashMessage } = useContext(UserContext);
@@ -23,22 +23,21 @@ export const SigninForm = () => {
      const handleSubmit = async (e) => {
         e.preventDefault();
         setFormSubmitted(true); 
-        handleLogin(formData.email, formData.pwd)
+        await handleLogin(formData.email, formData.pwd); // Wait for login process
      };
 
-     console.log("flashMessage:", flashMessage); 
-
     const toggleShowPassword = () => {
-        setShowPwd(!showPwd)
-    }
+        setShowPwd(!showPwd);
+    };
+
     return (
         <Box className="h-screen mx-auto" margin="10px">
             <h1 className='text-center text-pry text-3xl hover:text-carton font-extrabold pt-10 pb-10'>Sign In</h1>
-                {flashMessage && (
-                    <Box className={`text-${flashMessage.type} text-center my-3 text-black`}>
-                        {flashMessage.message}
-                    </Box>
-                )}
+            {flashMessage && (
+                <Box className={`text-${flashMessage.type} text-center my-3 text-black`}>
+                    {flashMessage.message}
+                </Box>
+            )}
             <form className="max-w-sm mx-auto w-full border rounded-xl border-green-700" onSubmit={handleSubmit}>
                 <Box className="flex flex-col pt-10">
                     <label htmlFor="email" className="text-gray-700">Email</label>
@@ -51,27 +50,29 @@ export const SigninForm = () => {
                         />
                     <label htmlFor="pwd" className="text-gray-700">Password</label>
                     <Box className="relative">
-                    <input 
-                        type={showPwd ? "text" : "password"} 
-                        name="pwd" 
-                        className="border border-black rounded-xl pr-32" 
-                        onChange={handleChange}
-                    />
+                        <input 
+                            type={showPwd ? "text" : "password"} 
+                            name="pwd" 
+                            className="border border-black rounded-xl pr-32" 
+                            onChange={handleChange}
+                        />
                         <button 
                             type="button" 
                             className="absolute inset-y-0 right-0 pr-2 flex items-center" 
-                            onClick={toggleShowPassword}> {showPwd ? 
-                            <VisibilityIcon /> : <VisibilityOffIcon />} 
+                            onClick={toggleShowPassword}
+                        >
+                            {showPwd ? <VisibilityIcon /> : <VisibilityOffIcon />} 
                         </button>
                     </Box>
-                    <Button type="submit" className="h-8 mt-5 text-white" onClick={handleLogin}>Sign In</Button>
+                    <Button type="submit" className="h-8 mt-5 text-white">Sign In</Button>
                 </Box>
                 <Box className="text-btn text-center my-3">
-                    <span style={{ fontSize: "smaller"}}>Don't have an account yet? &nbsp;
-                        <Link className="text-red-600" to="/admin/signin" style={{ textDecoration: "none" }}>Click Here</Link>
+                    <span style={{ fontSize: "smaller"}}>
+                        Not an admin? &nbsp;
+                        <Link className="text-red-600" to="/" style={{ textDecoration: "none" }}>Click Here</Link>
                     </span>
                 </Box>
             </form>
         </Box>
-    )
-}
+    );
+};
