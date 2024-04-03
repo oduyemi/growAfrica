f889634ba0eb9181d812a77e7a0a8c87394476e8
Yemi Oduyemi <yhermii@gmail.com>
2024-04-03 20:32:27 +0100

    user stored updated

--- src/UserContext.js
+++ src/UserContext.js
@@ -7,48 +7,48 @@ export const UserProvider = ({ children }) => {
   const [flashMessage, setFlashMessage] = useState(null);
   const [user, setUser] = useState(() => {
     const storedUser = localStorage.getItem("user");
-    return storedUser ? JSON.parse(storedUser) : {};
-  });
+    return storedUser;
+  }); 
 
-    // LOGIN VALIDATION
-    const handleLogin = async (email, password) => {
-      try {
-        const response = await axios.post("https://grow-africa-api.vercel.app/send/admin/signin", { email, password });
+
+  const handleLogin = async (email, password) => {
+    try {
+      const response = await axios.post("https://grow-africa-api.vercel.app/send/admin/signin", { email, password });
   
-        if (response.status === 200) {
-          console.log("Success:", response.data);
-          setUser(response.data.user);
-          localStorage.setItem("token", response.data.token);
-          localStorage.setItem("user", JSON.stringify(response.data.user));
-          setFlashMessage({
-            type: "success",
-            message: "Login Successful. Welcome Back!",
-          });
+      if (response.status === 200) {
+        console.log("Success:", response.data);
+        setUser(response.data.user);
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+        setFlashMessage({
+          type: "success",
+          message: `Login Successful. Welcome Back ${response.data.user.fname}`,
+        });
   
-          setTimeout(() => {
-            window.location.href = "/admin";
-          }, 1000);
-        } else if (response.status === 400) {
-          console.log("Error:", response.data);
-          setFlashMessage({ type: "error", message: "All fields are required!" });
-        } else {
-          console.error("Error:", response.data);
-          setFlashMessage({ type: "error", message: "An unexpected error occurred. Please try again later." });
-        }
-      } catch (error) {
-        console.error("Axios Error:", error);
-  
-        if (error.response) {
-          console.log("Response Data:", error.response.data);
-          setFlashMessage({ type: "error", message: error.response.data.detail || error.response.data.message });
-        } else if (error.request) {
-          console.error("Request Error:", error.request);
-          setFlashMessage({ type: "error", message: "No response received from the server. Please try again later." });
-        }
+        setTimeout(() => {
+          window.location.href = "/admin";
+        }, 1000);
+      } else if (response.status === 400) {
+        console.log("Error:", response.data);
+        setFlashMessage({ type: "error", message: "All fields are required!" });
+      } else {
+        console.error("Error:", response.data);
+        setFlashMessage({ type: "error", message: "An unexpected error occurred. Please try again later." });
       }
-    };
+    } catch (error) {
+      console.error("Axios Error:", error);
   
-    // SIGNOUT 
+      if (error.response) {
+        console.log("Response Data:", error.response.data);
+        setFlashMessage({ type: "error", message: error.response.data.detail || error.response.data.message });
+      } else if (error.request) {
+        console.error("Request Error:", error.request);
+        setFlashMessage({ type: "error", message: "No response received from the server. Please try again later." });
+      }
+    }
+  };
+  
+    
     const handleSignout = () => {
       localStorage.removeItem("user");
       localStorage.removeItem("token");
@@ -56,6 +56,7 @@ export const UserProvider = ({ children }) => {
       window.location.href = "/admin/signin";
     };
   
+    
 
   return (
     <UserContext.Provider value={{ user, setUser, flashMessage, handleLogin, handleSignout }}>
