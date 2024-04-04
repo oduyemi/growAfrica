8256c2da69a8d77198986680fd754989a70193f0
Yemi Oduyemi <yhermii@gmail.com>
2024-04-04 11:27:24 +0100

    edit2

--- src/components/PopupForm.jsx
+++ src/components/PopupForm.jsx
@@ -38,7 +38,7 @@ const PopupForm = ({ open, onClose }) => {
   return (
     <Modal isOpen={open} onRequestClose={onClose} className="Modal">
       <Box className="grid items-center justify-center bg-white bg-opacity-75">
-        <Box className="formbox bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full">
+        <Box className="formbox bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full py-5 md:py-10 mb-10 mt-3">
             <button
                 onClick={onClose}
                 className="absolute top-2 right-2 text-red-500 hover:text-red-700"
@@ -58,8 +58,8 @@ const PopupForm = ({ open, onClose }) => {
                 />
                 </svg>
             </button>
-          <Box className="py-5 px-5 md:px-10 my-10">
-            <Box className="text-center mb-3">
+          <Box className="py-3 px-5 md:px-10">
+            <Box className="text-center mb-2">
               <Typography
                 variant="h4"
                 className="text-2xl text-gray-900"
@@ -81,8 +81,8 @@ const PopupForm = ({ open, onClose }) => {
               </Typography>
             </Box>
             <form onSubmit={handleSubmit}>
-              <Box width="100%" maxWidth="400px" margin="auto">
-                <Box className="mb-2">
+              <Box maxWidth="400px">
+                <Box className="mb-1">
                   <label htmlFor="fullname" className="text-xs font-semibold px-1">Enter Full Name</label>
                   <input
                     type="text"
@@ -90,7 +90,7 @@ const PopupForm = ({ open, onClose }) => {
                     value={formData.fullname}
                     onChange={handleChange}
                     className="
-                      w-full px-3 py-2 rounded-xl
+                      w-full px-3 py-1 rounded-xl
                       border-2 border-gray-200 outline-none
                       focus:border-green-700
                     "
@@ -98,13 +98,13 @@ const PopupForm = ({ open, onClose }) => {
                     required
                   />
                 </Box>
-                <Box className="mb-2">
+                <Box className="mb-1">
                   <label htmlFor="email" className="text-xs font-semibold px-1">Enter Email Address</label>
                   <input
                     name="email"
                     type="text"
                     className="
-                      w-full px-3 py-2 rounded-xl
+                      w-full px-3 py-1 rounded-xl
                       border-2 border-gray-200 outline-none
                       focus:border-green-700
                     "
@@ -114,13 +114,13 @@ const PopupForm = ({ open, onClose }) => {
                     required
                   />
                 </Box>
-                <Box className="mb-2">
+                <Box className="mb-1">
                   <label htmlFor="phone" className="text-xs font-semibold px-1">Enter Phone Number</label>
                   <input
                     type="text"
                     name="phone"
                     className="
-                      w-full px-3 py-2 rounded-xl
+                      w-full px-3 py-1 rounded-xl
                       border-2 border-gray-200 outline-none
                       focus:border-green-700
                     "
@@ -130,13 +130,13 @@ const PopupForm = ({ open, onClose }) => {
                     required
                   />
                 </Box>
-                <Box className="mb-2">
+                <Box className="mb-1">
                   <label htmlFor="productInterest" className="text-xs font-semibold px-1">Product Interest</label>
                   <input
                     type="text"
                     name="productInterest"
                     className="
-                      w-full px-3 py-2 rounded-xl
+                      w-full px-3 py-1 rounded-xl
                       border-2 border-gray-200 outline-none
                       focus:border-green-700
                     "
@@ -146,12 +146,12 @@ const PopupForm = ({ open, onClose }) => {
                     required
                   />
                 </Box>
-                <Box className="mb-2">
+                <Box className="mb-1">
                   <label htmlFor="shopperOrVendor" className="text-xs font-semibold px-1">Shopper or Vendor</label>
                   <select
                     name="shopperOrVendor"
                     className="
-                      w-full px-3 py-2 rounded-xl
+                      w-full px-3 py-1 rounded-xl
                       border-2 border-gray-200 outline-none
                       focus:border-green-700
                     "
@@ -164,12 +164,12 @@ const PopupForm = ({ open, onClose }) => {
                     <option value="vendor">Vendor</option>
                   </select>
                 </Box>
-                <Box className="mb-2">
+                <Box className="mb-1">
                   <label htmlFor="contactPreference" className="text-xs font-semibold px-1">Preferred Communication Method</label>
                   <select
                     name="contactPreference"
                     className="
-                      w-full px-3 py-2 rounded-xl
+                      w-full px-3 py-1 rounded-xl
                       border-2 border-gray-200 outline-none
                       focus:border-green-700
                     "
@@ -184,12 +184,12 @@ const PopupForm = ({ open, onClose }) => {
                     <option value="whatsapp">WhatsApp</option>
                   </select>
                 </Box>
-                <Box className="mb-2">
+                <Box className="mb-1">
                   <label htmlFor="how" className="text-xs font-semibold px-1">How Did You Hear About Us?</label>
                   <select
                     name="how"
                     className="
-                      w-full px-3 py-2 rounded-xl
+                      w-full px-3 py-1 rounded-xl
                       border-2 border-gray-200 outline-none
                       focus:border-green-700
                     "
@@ -206,7 +206,7 @@ const PopupForm = ({ open, onClose }) => {
                     <option value="friend">Friend</option>
                   </select>
                 </Box>
-                <Box className="mb-2">
+                <Box className="mb-1">
                   <Button
                     type="submit"
                     className="
